e45d2ea6e8f4f6c3158ea44a34a62b3b5e1d78bd
Felipe <felip.goncalves@hotmail.com>
2026-08-18 22:27:05 -0300

    feat: Implementing BalanceEditor

---
+++ src/controllers/balance_editor.py
@@ -0,0 +1,13 @@
+from src.models.repositories.user_repository import UserRepository
+
+class BalanceEditor:
+    def __init__(self, user_repository: UserRepository) -> None:
+        self.__user_repository = user_repository
+
+    def edit(self, user_id: int, new_balance: float) -> dict:
+        self.__user_repository.edit_balance(user_id, new_balance)
+        return {
+            "type": "User",
+            "count": 1,
+            "new balance": new_balance
+        }
